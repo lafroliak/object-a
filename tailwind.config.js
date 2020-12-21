6533e3a8060606b30989e98c29c0ec4897700d8d
Alexei Tukachev <atukachev@spbtv.com>
2020-12-21 11:32:00 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
