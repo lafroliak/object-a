7d4e47a90c8c9f804a1245f38b8044a492249b66
Alexei Tukachev <atukachev@spbtv.com>
2021-05-17 10:52:12 +0300

    try stripe

--- src/lib/stripe/createClient.ts
+++ src/lib/stripe/createClient.ts
@@ -1,5 +1,8 @@
 import Stripe from 'stripe'
 
+/* eslint-disable */
+console.log(process.env.STRIPE_SECRET_KEY)
+
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2020-08-27',
 })
