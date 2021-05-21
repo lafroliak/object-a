a0e890a1edcf509a4e5b559d97166abe122c5d14
Alexei Tukachev <atukachev@spbtv.com>
2021-05-21 17:04:40 +0300

    return collapsable

--- src/components/CollapsableImage.tsx
+++ src/components/CollapsableImage.tsx
@@ -1,46 +1,35 @@
-import {
-  Dispatch,
-  memo,
-  SetStateAction,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from 'react'
+import { memo, useCallback, useEffect, useRef, useState } from 'react'
 import useSpring from 'react-use/lib/useSpring'
 
 import useCollapseImage from '~hooks/useCollapseImage'
-import useMediaQuery from '~hooks/useMediaQuery'
-import { ImageVariant } from '~lib/crystallize/types'
 import { Option } from '~typings/utils'
 
 import IfElse from './IfElse'
 
 type Props = {
-  image: Option<ImageVariant>
+  image: Option<string>
   placeholder: Option<string>
   isHovered?: boolean
   isTap?: boolean
   inverted?: boolean
-  setLoaded?: Dispatch<SetStateAction<boolean>>
 }
-
-function Collapsable({
+function CollapsableImage({
   image,
+  placeholder,
   isHovered = true,
   isTap = false,
   inverted = false,
-  setLoaded = () => {},
 }: Props) {
   const cref = useRef<HTMLCanvasElement>(null)
   const [min] = useState(() => Math.random() * (0.2 - 0.1) + 0.1)
+  const [loaded, setLoaded] = useState<boolean>(false)
 
   const onLoad = useCallback(() => {
     setLoaded(true)
   }, [])
 
   const [state, render] = useCollapseImage({
-    image: image?.url,
+    image,
     cref,
     sp: 48,
     start: inverted ? 1 : min,
@@ -76,28 +65,6 @@ function Collapsable({
     }
   }, [state, min, isHovered, isTap, inverted])
 
-  return <canvas className="absolute inset-0" ref={cref} />
-}
-
-function CollapsableImage(props: Props) {
-  const { image, placeholder } = props
-  const [loaded, setLoaded] = useState<boolean>(false)
-  const isTouchScreen = useMediaQuery('isTouchScreen')
-  const isSM = useMediaQuery('isSM')
-  const hide = isTouchScreen || isSM
-
-  useEffect(() => {
-    if (hide && image?.url) {
-      let img: HTMLCanvasElement | HTMLImageElement = document.createElement(
-        'img',
-      )
-      img.onload = () => {
-        setLoaded(true)
-      }
-      img.src = image?.url
-    }
-  }, [hide, image])
-
   if (!image) return null
 
   return (
@@ -110,21 +77,7 @@ function CollapsableImage(props: Props) {
           />
         )}
       </IfElse>
-      <IfElse
-        predicate={!hide}
-        placeholder={
-          <div className="absolute inset-0 grid overflow-hidden place-items-center">
-            <img
-              src={image.url}
-              alt=""
-              width={image.width}
-              height={image.height || image.width}
-            />
-          </div>
-        }
-      >
-        {() => <Collapsable {...props} setLoaded={setLoaded} />}
-      </IfElse>
+      <canvas className="absolute inset-0" ref={cref} />
     </>
   )
 }
